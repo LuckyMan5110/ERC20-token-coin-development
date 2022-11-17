2581ed2b35e711e4fdcfaeef2c6e08311077d7ba
LuckyMan <98742466+LuckyMan5110@users.noreply.github.com>
2022-11-17 08:22:36 +0100

    Update tc-stage-3.sol

--- coin_stage3/contracts/tc-stage-3.sol
+++ coin_stage3/contracts/tc-stage-3.sol
@@ -20,12 +20,7 @@ contract admined {
 
 contract TCoin {
 
-	mapping (address => uint256) public balanceOf;
-	mapping (address => mapping (address => uint256)) public allowance;
-	// balanceOf[address] = 5;
-	string public standard = "TCoin v1.0";
-	string public name;
-	string public symbol;
+	
 	uint8 public decimals; 
 	uint256 public totalSupply;
 	event Transfer(address indexed from, address indexed to, uint256 value);
